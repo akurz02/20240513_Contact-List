3fdc3d8f1b5bada518263da073a5bbbe7ad4cc12
akurz02 <161508355+akurz02@users.noreply.github.com>
2024-05-24 16:27:50 +0000

    progressing on project

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,73 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: [],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+
+			loadAgendaContacts: async () => {
+				const response = await fetch("https://playground.4geeks.com/contact/agenda/akurz02");
+				if(!response.ok) {
+					throw new Error(response.status, response.statusText)
+				}
+				const data = await response.json();
+				setStore({contacts: data.contacts});
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+
+			contactCreate: async (name,phone,email,address) => {
 				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+				const response = await fetch(`https://playground.4geeks.com/contact/agendas/akurz02/contacts/`, {
+					method: "POST",
+					headers: { "Content-Type": "application/json"},
+					body: JSON.stringify({
+						full_name: name,
+						phone: phone,
+						email: email,
+						address: address,
+						agenda_slug: "akurz02",
+					}),
 				});
-
-				//reset the global store
-				setStore({ demo: demo });
+				if(!response.ok) {
+					throw new Error(response.status, response.statusText)
+				}
+				
+				getActions().loadAgendaContacts();
+				const data = await response.json();
+		  		setStore({ contacts: [...store.contacts, data] });
+			},
+			
+			deleteContact: async (id) => {
+				const store = getStore();
+				const response = await fetch(
+				  "https://playground.4geeks.com/contact/agendas/akurz02/contacts/" + id,
+				  {
+					method: "DELETE",
+					headers: { "Content-Type": "application/json" },
+				  }
+				);
+				const data = await response.json();
+				setStore({
+				  contacts: store.contacts.filter((contact) => contact.id !== id),
+				});
+			},
+			editContact: async (id, name, phone, email, address) => {
+				const store = getStore();
+				const response = await fetch(
+				  "https://playground.4geeks.com/contact/agendas/akurz02/contacts/" + id,
+				  {
+					method: "PUT",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+					  full_name: name,
+					  phone: phone,
+					  email: email,
+					  address: address,
+					  agenda_slug: "akurz02",
+					}),
+				  }
+				);
+				const data = await response.json();
+				setStore({ contacts: [...store.contacts, data] });
 			}
 		}
 	};
